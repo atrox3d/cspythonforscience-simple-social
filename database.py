91222e1824286090300ce7636b39b76c53e3ef95
atrox3d <atrox3d@gmail.com>
2024-08-04 14:48:10 +0200

    add post model

--- database.py
+++ database.py
@@ -2,46 +2,54 @@ from multiprocessing import current_process
 import sqlite3
 from contextlib import closing
 
+from models import Post, Posts
+
 def dict_factory(connection, row) -> dict:
     return {
             col[0]: row[idx] 
             for idx, col in enumerate(connection.description)
     }
 
-def get_posts(connection:sqlite3.Connection) -> list[dict]:
+def get_posts(connection:sqlite3.Connection) -> Posts:
     # connection.row_factory = sqlite3.Row
     connection.row_factory = dict_factory
     
-    with closing(connection.cursor()) as cur:
-        cursor = cur.execute(
-            '''
-            SELECT post_title, post_text, user_id
-            FROM posts
-            '''
-        )
-        return [row for row in cursor.fetchall()]
+    with connection:
+        with closing(connection.cursor()) as cur:
+            cursor = cur.execute(
+                '''
+                SELECT post_title, post_text, user_id
+                FROM posts
+                '''
+            )
+            return Posts(posts=[Post.model_validate(row) for row in cursor.fetchall()])
+            # return [Post.model_validate(row) for row in cursor.fetchall()]
 
 
-def insert_post(connection:sqlite3.Connection, post:dict):
-    with closing(connection.cursor()) as cur:
-        cur.execute(
-            '''
-            INSERT INTO posts
-            (post_title, post_text, user_id)
-            VALUES
-            (:post_title, :post_text, :user_id)
-            ''',
-            post
-        )
+def insert_post(connection:sqlite3.Connection, post:Post):
+    with connection:
+        with closing(connection.cursor()) as cur:
+            cur.execute(
+                '''
+                INSERT INTO posts
+                (post_title, post_text, user_id)
+                VALUES
+                (:post_title, :post_text, :user_id)
+                ''',
+                post.model_dump()
+            )
+        # connection.commit()
 
 if __name__ == '__main__':
     connection = sqlite3.connect('social.db')
 
-    # test_post = {
-        # 'post_title': 'first post',
-        # 'post_text': 'another this is a test',
-        # 'user_id': 1
-    # }
-# 
-    # insert_post(connection, test_post)
+    test_post = {
+        'post_title': 'first pydantic post',
+        'post_text': 'pydantic post',
+        'user_id': 1
+    }
+
+    test_post = Post(**test_post)
+
+    insert_post(connection, test_post)
     print(get_posts(connection))
---
+++ models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class Post(BaseModel):
+    post_title: str
+    post_text: str
+    user_id: int
+
+class Posts(BaseModel):
+    posts: list[Post]
+
