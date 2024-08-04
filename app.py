5ae415f63a825567210a51042db9dcfcb0f99bc2
atrox3d <atrox3d@gmail.com>
2024-08-04 15:24:16 +0200

    add routes

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from sqlite3 import Connection, Row
+
+from database import get_posts, dict_factory, insert_post
+from models import Post, Posts
 
 app = FastAPI()
+connection = Connection('social.db')
+connection.row_factory = dict_factory
 
 @app.get('/')
 async def root():
     return {'message': 'server running'}
+
+@app.get('/posts')
+async def posts():
+    return get_posts(connection)
+
+@app.post('/posts')
+async def posts(post:Post) -> Post:
+    insert_post(connection, post)
+    return post
+
+
