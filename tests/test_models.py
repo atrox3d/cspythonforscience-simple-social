abcf382da7bb84aed023aaece18120fbf4638e81
atrox3d <atrox3d@gmail.com>
2024-08-05 05:44:53 +0200

    rename test functions

--- tests/test_models.py
+++ tests/test_models.py
@@ -20,20 +20,20 @@ def ten_posts(post: Post) -> list[Post]:
     return [post for _ in range(10)]
 
 
-def test_post(post: Post):
+def test_post_attributes(post: Post):
     assert post.post_title == 'title'
     assert post.post_text == 'text'
     assert post.user_id == 0
 
-def test_post_json(post_json: dict):
+def test_post_json_attributes(post_json: dict):
     assert post_json['post_title'] == 'title'
     assert post_json['post_text'] == 'text'
     assert post_json['user_id'] == 0
 
-def test_post_str(post:Post):
+def test_post_to_string(post:Post):
     assert str(post) == "post_title='title' post_text='text' user_id=0"
 
-def test_repr_post(post: Post):
+def test_post_to_repr(post: Post):
     assert repr(post) == "Post(post_title='title', post_text='text', user_id=0)"
 
 def test_post_equality(post: Post):
